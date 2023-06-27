da0de4404a374a4ad2d9524e00d40f835ae48021
kevin <kevindiegodasilvasousa@gmail.com>
2023-06-27 01:19:29 -0300

    chore: Finalizado projeto criando estilos e build

---
+++ README.md
@@ -0,0 +1,32 @@
+# Tasks Lists
+
+Com intuito em fazer o uso do hook UseReducer, desenvolvi esta simples aplicação na qual adiciona uma lista de tarefas onde poderão ser marcadas como conclúidas para um auxilio pessoal do usuário.
+
+![](./github/gif.gif)
+
+---
+# Desenvolvimento
+
+[react](https://react.dev/) <br/>
+[tailwindcss](https://tailwindcss.com/) <br/>
+[typescript](https://www.typescriptlang.org/) <br/>
+
+---
+
+# Como usar
+
+Para testar o software será necessário baixar o repositório em sua máquina e utilizar o comando ```npm install``` + ```npm run dev```
+
+---
+
+# Layout
+
+![Image](./github/desktop.png)
+
+![Image](./github/mobile.png)
+
+--- 
+
+# Contribuidores
+
+<img src="https://github.com/kevinDsousa.png" style="width: 60px; border-radius: 50%"/>
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,22 @@
-import './App.css'
-import { useReducer, useState } from 'react'
+import './App.css';
+import { useReducer, useState } from 'react';
+import computer from './assets/computer.jpg'
 
-const reducer = (state, action) => {
+interface Task {
+  name: string;
+  isCompleted: boolean;
+}
+
+interface AppState {
+  tasks: Task[];
+  tasksCount: number;
+}
+
+type AppAction =
+  | { type: 'add-task'; payload: string }
+  | { type: 'toggle-task'; payload: number };
+
+const reducer = (state: AppState, action: AppAction): AppState => {
   switch (action.type) {
     case 'add-task':
       return {
@@ -11,46 +26,69 @@ const reducer = (state, action) => {
     case 'toggle-task':
       return {
         ...state,
-        tasks: state.tasks.map((item, index) => index === action.payload ? { ...item, isCompleted: !item.isCompleted} : item)
-      }
+        tasks: state.tasks.map((item, index) =>
+          index === action.payload ? { ...item, isCompleted: !item.isCompleted } : item
+        ),
+      };
     default:
       return state;
   }
-}
+};
 
 function App() {
-
-  const [state, dispatch] = useReducer(reducer, { tasks: [], tasksCount: 0 })
-  const [inputValue, setInputValue] = useState('')
+  const [state, dispatch] = useReducer(reducer, { tasks: [], tasksCount: 0 });
+  const [inputValue, setInputValue] = useState('');
 
   return (
-    <section className='bg-slate-900 w-full h-full text-cyan-50 flex flex-col items-center justify-center'>
-      <h2 className='text-2xl font-bold mt-3'>To do list</h2>
+    <section className='flex flex-1 font-mono max-sm:flex-col'>
+      <section className='bg-opacity-25 bg-slate-600 w-1/2 h-screen max-sm:w-full max-sm:h-1/2 text-cyan-50 flex flex-col items-center border-2 border-zinc-500'>
+        <h2 className='text-4xl font-bold py-5 max-sm:text-2xl'>To do list</h2>
 
-      <div className='flex flex-1 justify-center items-center gap-2 mt-8'>
-        <p className='text-zinc-300'>Descreva sua tarefa</p>
-        <input className='bg-zinc-500 rounded px-6 py-2 outline-none text-white' type="text" id="" value={inputValue} onChange={e => setInputValue(e.target.value)} />
-        <button
-          onClick={() => {
-            dispatch({ type: 'add-task', payload: inputValue })
-            setInputValue("")
-          }}>Adicionar</button>
-      </div>
-      <div>
-        {state.tasks.map((task, index) => (
-          <p
-            key={index}
-            onClick={() => dispatch({ type: 'toggle-task', payload: index })}
-            style={task.isCompleted ? { textDecoration: 'line-through' } : {}}
-            className=''
+        <p className='text-center text-sm text-green-400'>Informe a sua tarefa no campo de digitação e clique em Adicionar</p>
+
+        <div className='flex flex-col justify-center items-center gap-2 my-4'>
+          <p className='text-zinc-300 font-semibold'>Descreva sua tarefa</p>
+          <input
+            className='bg-zinc-500 rounded px-6 py-2 outline-none text-white placeholder:text-white placeholder:text-sm focus:bg-zinc-400'
+            type='text'
+            id=''
+            placeholder='Adicione uma tarefa aqui'
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button
+            className='bg-slate-200 hover:bg-green-400 hover:text-white outline-none text-zinc-900 rounded w-full h-10'
+            onClick={() => {
+              dispatch({ type: 'add-task', payload: inputValue });
+              setInputValue('');
+            }}
           >
-            {task.name}
-          </p>
-        ))}
-
-      </div>
+            Adicionar
+          </button>
+          <div className='mt-11'>
+            <img
+              src={computer} alt="computer"
+              className='rounded-full w-60 h-60 border-2 border-slate-950 max-sm:hidden' />
+          </div>
+        </div>
+      </section>
+      <section className='w-1/2 max-sm:w-full max-sm:h-1/2 text-green-400'>
+        <div className='flex flex-col items-center justify-center my-4 gap-2'>
+          <h2 className='text-white font-bold text-3xl my-4 max-sm:text-2xl'>Atividades adicionadas</h2>
+          {state.tasks.map((task, index) => (
+            <p
+              key={index}
+              onClick={() => dispatch({ type: 'toggle-task', payload: index })}
+              style={task.isCompleted ? { textDecoration: 'line-through', color: 'red' } : {}}
+              className='text-lg border-2 w-2/3 h-8 pl-1 rounded cursor-pointer text-center truncate'
+            >
+              {task.name}
+            </p>
+          ))}
+        </div>
+      </section>
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
